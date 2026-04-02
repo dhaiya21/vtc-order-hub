bfd1a374ced2dc7581b4c75622751adf495779e5
Vikas Kumar <142376528+dhaiya21@users.noreply.github.com>
2026-04-02 15:39:14 +0530

    Delete backend directory

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = 'vtc-secret-key-2024'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database/vtc_orders.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = 'uploads'
